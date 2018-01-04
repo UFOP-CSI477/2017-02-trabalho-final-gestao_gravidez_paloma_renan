f63bcfb5e28674a49349f5d4efaa2fe222d9ee7c
RenanCn <renan.rcn@hotmail.com>
2018-01-04 18:36:47 -0200

    -

--- gestaoGravidez/js/interface_gestacao.js
+++ gestaoGravidez/js/interface_gestacao.js
@@ -58,7 +58,25 @@ jQuery(document).ready(function($) {
     $("#grupo-vacinas").hide();
     $("#grupo-consultas").hide();
     $("#grupo-diario").show();
-  });
+  }); // fim controle de interface do menu
 
+  /* ############################################### */
 
+  $("button[name='cadastrar-consulta']").click(function(event) {
+
+    if(!$("input[name='data-consulta']").val())
+    {
+      $("#grupo-consultas").addClass("has-error");
+      $("#alertaConsultas").show();
+      $("input[name='data-consulta']").val("");
+      $("input[name='data-consulta']").focus();
+      return;
+    }
+
+    $("#grupo-consultas").removeClass("has-error");
+    $("#alertaConsultas").hide();
+
+  });// fim
+
+  
 });
