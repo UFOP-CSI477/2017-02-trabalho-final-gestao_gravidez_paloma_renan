5f40c3284773ce94973685f860a1266b0c360ea8
RenanCn <renan.rcn@hotmail.com>
2018-01-05 18:45:39 -0200

    erros/debug

--- gestaoGravidez/js/interface_gestacao.js
+++ gestaoGravidez/js/interface_gestacao.js
@@ -60,7 +60,7 @@ jQuery(document).ready(function($) {
     $("#grupo-diario").show();
   }); // fim controle de interface do menu
 
-  /* ############################################### */
+  /* [Erros - Consultas] */
 
   $("button[name='cadastrar-consulta']").click(function(event) {
 
@@ -102,6 +102,205 @@ jQuery(document).ready(function($) {
     $("#grupo-info-gestacao").show();
 
   });
+
+  /*[Erros - Vacinas]*/
+
+  $("button[name='cadastrar-vacina']").click(function(event) {
+
+    if(!$("input[name='data-vacina']").val())
+    {
+      $("#alertaVacinas-data").slideDown();
+      $("input[name='data-vacina']").val("");
+      $("input[name='data-vacina']").focus();
+      return;
+    }
+
+    $("#alertaVacinas-data").hide();
+
+    ////
+
+    if(!$("input[name='horario-vacina']").val())
+    {
+      $("#alertaVacinas-horario").slideDown();
+      $("input[name='horario-vacina']").val("");
+      $("input[name='horario-vacina']").focus();
+      return;
+    }
+
+    $("#alertaVacinas-horario").hide();
+
+    ////
+
+    if(!$("input[name='nome-vacina']").val())
+    {
+      $("#alertaVacinas-nome").slideDown();
+      $("input[name='nome-vacina']").focus();
+      $("input[name='nome-vacina']").val("");
+      return;
+    }
+
+    $("#alertaVacinas-nome").hide();
+
+    ////
+
+    $("#alertaVacinas-medico").hide();
+
+    $("#grupo-vacinas").hide();
+    $("#grupo-info-gestacao").show();
+
+  });
+
+  /* [Erros - Humor] */
+
+  $("button[name='cadastrar-humor']").click(function(event) {
+
+    if(!$("input[name='data-humor']").val())
+    {
+      $("#alertaHumor-data").slideDown();
+      $("input[name='data-humor']").val("");
+      $("input[name='data-humor']").focus();
+      return;
+    }
+
+    $("#alertaHumor-data").hide();
+
+    ////
+
+    if(!$("input[name='radio']:checked").val())
+    {
+      $("#alertaHumor-sentimento").slideDown();
+      return;
+    }
+
+    $("#alertaHumor-sentimento").hide();
+
+    ////
+
+    $("#grupo-humor").hide();
+    $("#grupo-info-gestacao").show();
+
+  });
+
+    /* [Erros - Desejos] */
+
+  $("button[name='cadastrar-desejo']").click(function(event) {
+
+    if(!$("input[name='data-desejo']").val())
+    {
+      $("#alertaDesejos-data").slideDown();
+      $("input[name='data-desejo']").val("");
+      $("input[name='data-desejo']").focus();
+      return;
+    }
+
+    $("#alertaDesejos-data").hide();
+
+    ////
+
+    if(!$("textarea[name='descricao-desejo']").val())
+    {
+      $("#alertaDesejos-descricao").slideDown();
+      $("input[name='descricao-desejo']").val("");
+      $("input[name='descricao-desejo']").focus();
+      return;
+    }
+
+    $("#alertaDesejos-descricao").hide();
+
+    ////
+
+    $("#grupo-desejos").hide();
+    $("#grupo-info-gestacao").show();
+
+  });
+
+  /* [Erros - Album] */
+
+$("button[name='cadastrar-album']").click(function(event) {
+
+  if(!$("input[name='data-album']").val())
+  {
+    $("#alertaAlbum-data").slideDown();
+    $("input[name='data-album']").val("");
+    $("input[name='data-album']").focus();
+    return;
+  }
+
+  $("#alertaAlbum-data").hide();
+
+  ////
+
+  if(!$("input[name='arquivo-album']").val())
+  {
+    $("#alertaAlbum-arquivo").slideDown();
+    $("input[name='arquivo-album']").val("");
+    $("input[name='arquivo-album']").focus();
+    return;
+  }
+
+  $("#alertaAlbum-arquivo").hide();
+
+  ////
+
+  $("#grupo-album").hide();
+  $("#grupo-info-gestacao").show();
+
+});
+
+  /* [Erros - Diario] */
+
+$("button[name='cadastrar-diario']").click(function(event) {
+
+  if(!$("input[name='data-diario']").val())
+  {
+    $("#alertaDiario-data").slideDown();
+    $("input[name='data-diario']").val("");
+    $("input[name='data-diario']").focus();
+    return;
+  }
+
+  $("#alertaDiario-data").hide();
+
+  ////
+
+  if(!$("input[name='peso-diario']").val() || isNaN($("input[name='peso-diario']").val()))
+  {
+    $("#alertaDiario-peso").slideDown();
+    $("input[name='peso-diario']").val("");
+    $("input[name='peso-diario']").focus();
+    return;
+  }
+
+  $("#alertaDiario-peso").hide();
+
+  ////
+
+  if(!$("input[name='altura-diario']").val() || isNaN($("input[name='altura-diario']").val()))
+  {
+    $("#alertaDiario-altura").slideDown();
+    $("input[name='altura-diario']").val("");
+    $("input[name='altura-diario']").focus();
+    return;
+  }
+
+  $("#alertaDiario-altura").hide();
+
+  ////
+
+  $("#grupo-diario").hide();
+  $("#grupo-info-gestacao").show();
+
+});
+
+
+
+
+
+
+
+
+
+
 
 
 });
