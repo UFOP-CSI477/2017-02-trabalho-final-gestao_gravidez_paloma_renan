6d79973e5df7dd9824d9cea818c9a03697dec1f1
RenanCn <renan.rcn@hotmail.com>
2018-01-08 12:42:05 -0200

    relatorio

--- gestaoGravidez/js/interface_relatorio.js
+++ gestaoGravidez/js/interface_relatorio.js
@@ -1,3 +1,15 @@
 jQuery(document).ready(function($) {
-  
+
+  $("button[name='gestacao']").click(function(event) {
+    $("#grupo-info-relatorio").hide();
+    $("#grupo-posparto").hide();
+    $("#grupo-gestacao").show();
+  });
+
+  $("button[name='posparto']").click(function(event) {
+    $("#grupo-info-relatorio").hide();
+    $("#grupo-gestacao").hide();
+    $("#grupo-posparto").show();
+  });
+
 });
